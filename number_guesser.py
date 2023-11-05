0634a92d797109df7596891d444f2ea16d432c25
Sahar <saharhalim151@gmail.com>
2023-11-05 18:54:21 -0500

    get input from the user

--- number_guesser.py
+++ number_guesser.py
@@ -1,2 +1,10 @@
 import random
-random.randrange()
+
+
+top_of_range = input("Type a number: ")
+
+if top_of_range.isdigit():
+  top_of_range = int(top_of_range)
+
+random_number = random.randint(11)
+
