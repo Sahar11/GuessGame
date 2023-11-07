cd7ef548834645e45f6d6df7df690e6be45e7b4d
Sahar <saharhalim151@gmail.com>
2023-11-06 21:46:24 -0500

    put if statement for number

--- number_guesser.py
+++ number_guesser.py
@@ -6,5 +6,11 @@ top_of_range = input("Type a number: ")
 if top_of_range.isdigit():
   top_of_range = int(top_of_range)
 
-random_number = random.randint(11)
+  if top_of_range <= 0:
+    print("Please type a number larger than 0 next time. ")
+    quit()
+else:
+  print('Please type a number next time. ')
+
+random_number = random.randint(0, top_of_range)
 
